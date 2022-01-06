1025a966d093485ad5335c0b5a09d3b79120b225
manasa.kaniselvan <mkanisel@uwaterloo.ca>
2022-01-05 21:25:28 -0500

    part2 added

--- day_7/Day_7_Treachery_of_Whales.py
+++ day_7/Day_7_Treachery_of_Whales.py
@@ -19,6 +19,18 @@ if __name__ == '__main__':
         if fuel_cost < min_fuel:
             min_fuel = fuel_cost
 
-    print(f'Min Fuel is {min_fuel}')
+    print(f'Part 1 Min Fuel is {min_fuel}')
 
     # Part 2:
+
+    min_fuel = float('inf')
+    fuel_cost = 0
+
+    for aligned_pos in range(max(crab_positions)+1):
+        difference = abs(crab_positions - aligned_pos)
+        fuel_cost = sum((1 + d)*d/2 for d in difference)
+
+        if fuel_cost < min_fuel:
+            min_fuel = fuel_cost
+
+    print(f'Part 2 Min Fuel is {min_fuel}')
